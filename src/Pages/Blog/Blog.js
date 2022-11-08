9afa7951cd7a5fb1bca3841d4f81a5d566232bc8
uddinmayraj <mayrajuddin03@gmail.com>
2022-11-08 22:07:33 +0600

    blog page added

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -2,8 +2,40 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <h3>this is blog page</h3>
+        <div className='container mx-auto my-9'>
+            <h3 className='text-4xl pb-6 uppercase text-center font-bold'>blog </h3>
+            <div tabIndex={0} className="w-3/4 mx-auto collapse collapse-arrow border border-base-300  rounded-box mb-4">
+                <div className="collapse-title text-xl font-medium tealish-blue-bg text-white">
+                    1. Difference between SQL and NoSQL
+                </div>
+                <div className="collapse-content">
+                    <p tabIndex={0} className='text-neutral-800 pt-3 '> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis adipisci, optio saepe temporibus impedit minus? Pariatur est molestiae similique? Expedita, unde. Nemo commodi et cum ipsum placeat officiis quidem sit.</p>
+                </div>
+            </div>
+            <div tabIndex={0} className="w-3/4 mx-auto collapse collapse-arrow border border-base-300  rounded-box mb-4">
+                <div className="collapse-title text-xl font-medium tealish-blue-bg text-white">
+                    2. What is JWT, and how does it work?
+                </div>
+                <div className="collapse-content">
+                    <p tabIndex={0} className='text-neutral-800 pt-3'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis adipisci, optio saepe temporibus impedit minus? Pariatur est molestiae similique? Expedita, unde. Nemo commodi et cum ipsum placeat officiis quidem sit.</p>
+                </div>
+            </div>
+            <div tabIndex={0} className="w-3/4 mx-auto collapse collapse-arrow border border-base-300  rounded-box mb-4">
+                <div className="collapse-title text-xl font-medium tealish-blue-bg text-white">
+                    3. What is the difference between javascript and NodeJS?
+                </div>
+                <div className="collapse-content">
+                    <p tabIndex={0} className='text-neutral-800 pt-3'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis adipisci, optio saepe temporibus impedit minus? Pariatur est molestiae similique? Expedita, unde. Nemo commodi et cum ipsum placeat officiis quidem sit.</p>
+                </div>
+            </div>
+            <div tabIndex={0} className="w-3/4 mx-auto collapse collapse-arrow border border-base-300  rounded-box mb-4">
+                <div className="collapse-title text-xl font-medium tealish-blue-bg text-white">
+                    4. How does NodeJS handle multiple requests at the same time?
+                </div>
+                <div className="collapse-content">
+                    <p tabIndex={0} className='text-neutral-800 pt-3'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis adipisci, optio saepe temporibus impedit minus? Pariatur est molestiae similique? Expedita, unde. Nemo commodi et cum ipsum placeat officiis quidem sit.</p>
+                </div>
+            </div>
         </div>
     );
 };
